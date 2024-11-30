93cfd17fd8f43fcdc9e7f7f9b10f240abb5a4a82
oliverisyes <152007306+oliverisyes@users.noreply.github.com>
2024-11-30 16:06:44 +0000

    Beginnings of dialogue

---
+++ Cat Jam/Assets/Scripts/DialogueControl.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialougeControl : MonoBehaviour 
+{
+    [SerializeField] TextMeshProUGUI dialougeText;
+    //[SerializeField] TextMeshProUGUI nameText;
+    [SerializeField] GameObject dialougePanel;
+
+    public void ShowDialouge(string dialouge)
+    {
+        Debug.Log(dialouge);
+        //nameText.text = name + "...";
+        dialougeText.text = dialouge;
+        dialougePanel.SetActive(true);
+    }
+
+    public void EndDialouge()
+    {
+        //nameText.text = null;
+        dialougeText.text = null; ;
+        dialougePanel.SetActive(false);
+    }
+}
