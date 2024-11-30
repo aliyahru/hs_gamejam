e4fc01a6ffa9522dd473e5b8c4b6e3b73f30b157
taylorvelvet <161587587+taylorvelvet@users.noreply.github.com>
2024-11-30 16:27:28 +0000

    a

--- Cat Jam/Assets/Scripts/Circle.cs
+++ Cat Jam/Assets/Scripts/Circle.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEditor;
 using UnityEngine;
 using UnityEngineInternal;
 
@@ -10,23 +13,62 @@ public class Circle : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Vector2 lastDir;
+
+    Boolean isMoving = false;
     public float runSpeed = 20.0f;
+
+    Vector2 stopped;
 
 
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        lastDir = new Vector2(0, 0);
+        stopped = new Vector2(0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+        // if (Raytest()) {
+        //     UnityEngine.Debug.Log("O");
+        //     // UnityEngine.Debug.Log("raytest");
+        // }
+        if (Input.GetKeyDown("space")) {
+            UnityEngine.Debug.Log("space");
+            if (Raytest()) {
+                UnityEngine.Debug.Log("helllo i am the spirit");
+            }
+        }
+        
     }
 
     private void FixedUpdate() {
         body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        
+        if (body.velocity != stopped) {
+            lastDir = body.velocity;
+        }
+        // UnityEngine.Debug.Log(body.velocity);
+
     }
     
+    public Boolean Raytest() {
+        float raydist = 2f;
+        RaycastHit2D foundHit = Physics2D.Raycast(transform.position, lastDir, raydist, LayerMask.GetMask("Interactable"));
+        UnityEngine.Debug.DrawRay(transform.position, lastDir * raydist, Color.white, 0);
+
+        if (foundHit.collider != null) {
+            UnityEngine.Debug.Log(body.velocity);
+            if (foundHit.collider.gameObject.tag == "Spirit") {
+                // UnityEngine.Debug.Log("hellooo i am the spirit");
+                return true;
+            }
+        }
+        return false;
+    }
 }
