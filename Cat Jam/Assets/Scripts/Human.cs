0d1dbfa08582102d51864a9bf6cccf2c85048262
taylorvelvet <161587587+taylorvelvet@users.noreply.github.com>
2024-12-01 11:25:17 +0000

    first

---
+++ Cat Jam/Assets/Scripts/Human.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class HUman : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Collider2D effect_area;
+    void Start()
+    {
+        effect_area = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // List<Collider2D> results = new List<Collider2D>();
+        // ContactFilter2D filter = new ContactFilter2D();
+        // LayerMask Mask = LayerMask.GetMask("Cat");
+        // filter.layerMask = Mask;
+        // // filter.SetLayerMask(true);
+        // if (Physics2D.OverlapCollider(effect_area, filter, results) > 1) {
+
+        //     UnityEngine.Debug.Log(Physics2D.OverlapCollider(effect_area, filter, results));
+        // }
+    }
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        UnityEngine.Debug.Log("hold on...");
+        if (collider.gameObject.tag == "Cat") {
+            UnityEngine.Debug.Log("you faill");
+        }
+    }
+}
