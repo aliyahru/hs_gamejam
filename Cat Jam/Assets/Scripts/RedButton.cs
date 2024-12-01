8e73d3af52158d6f0132be8cad1ca5390bab9723
oliverisyes <152007306+oliverisyes@users.noreply.github.com>
2024-12-01 10:01:35 +0000

    BUTTON

--- Cat Jam/Assets/Scripts/RedButton.cs
+++ Cat Jam/Assets/Scripts/RedButton.cs
@@ -7,6 +7,7 @@ public class RedButton : MonoBehaviour
 {
     public DialougeAsset dialougeAsset;
     public DialougeControl control;
+    bool button = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +18,10 @@ public class RedButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("r") && button == true)
+        {
+            control.EndDialouge();
+        }
     }
 
     public Boolean OnPress(GameObject pressObject) {
@@ -25,8 +29,8 @@ public class RedButton : MonoBehaviour
             UnityEngine.Debug.Log("Object is too light!");
             return false;
         } else {
-            UnityEngine.Debug.Log("Wow! stupid fffuckin cat");
             control.ShowDialouge(6);
+            button = true;
             return true;
         }
     }
